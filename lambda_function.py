8b1c0a38ab352f0b0f072df93abc218f3b5e19e3
Elinuel1 <adzimanuel@gmail.com>
2025-08-24 11:17:43 +0000

    innitial AWS Lambda and API project commit

---
+++ lambda_function.py
@@ -0,0 +1,5 @@
+def lambda_handler(event, context):
+    return {
+        "statusCode": 200,
+        "body": "This project has given me the opportunity to learn and grow using AWS Lambda and API!"
+    }
---
+++ main.tf
@@ -0,0 +1,80 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# IAM role for Lambda
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_exec_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+# Attach policy so Lambda can write logs
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+# Zip the Lambda code
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_file = "${path.module}/lambda_function.py"
+  output_path = "${path.module}/lambda_function.zip"
+}
+
+# Lambda function
+resource "aws_lambda_function" "hello_lambda" {
+  function_name = "helloLambda"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.9"
+
+  filename         = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+}
+
+# API Gateway
+resource "aws_apigatewayv2_api" "http_api" {
+  name          = "HelloAPI"
+  protocol_type = "HTTP"
+}
+
+# API Integration with Lambda
+resource "aws_apigatewayv2_integration" "lambda_integration" {
+  api_id                 = aws_apigatewayv2_api.http_api.id
+  integration_type       = "AWS_PROXY"
+  integration_uri        = aws_lambda_function.hello_lambda.invoke_arn
+  payload_format_version = "2.0"
+}
+
+# API Route
+resource "aws_apigatewayv2_route" "hello_route" {
+  api_id    = aws_apigatewayv2_api.http_api.id
+  route_key = "GET /hello"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+}
+
+# Stage
+resource "aws_apigatewayv2_stage" "default" {
+  api_id      = aws_apigatewayv2_api.http_api.id
+  name        = "$default"
+  auto_deploy = true
+}
+
+# Allow API Gateway to invoke Lambda
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.hello_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.http_api.execution_arn}/*/*"
+}
